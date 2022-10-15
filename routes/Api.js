2146777fea143ebc8db17c133d641605e58edc31
Rory Pearson <52637194+Mezmerizxd@users.noreply.github.com>
2022-10-15 13:55:21 +0100

    get friends messages

--- routes/Api.js
+++ routes/Api.js
@@ -4,6 +4,7 @@ const SignUp = require('../controllers/authentication/signup');
 const Login = require('../controllers/authentication/login');
 const GetFriends = require('../controllers/application/get-friends');
 const GetFriendRequests = require('../controllers/application/get-friend-requests');
+const GetFriendMessages = require('../controllers/application/get-friends-messages');
 
 module.exports = (express) => {
     express.post('/api/test', Test.perform);
@@ -11,4 +12,5 @@ module.exports = (express) => {
     express.get('/api/auth/login', Login.perform);
     express.get('/api/app/get-friends', GetFriends.perform);
     express.get('/api/app/get-friend-requests', GetFriendRequests.perform);
+    express.get('/api/app/get-friends-messages', GetFriendMessages.perform);
 };
