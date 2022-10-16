8f3e4289ec96d71264f85e289abdefcbca47b24f
Rory Pearson <52637194+Mezmerizxd@users.noreply.github.com>
2022-10-16 13:21:47 +0100

    Update Statics.js

--- middlewares/Statics.js
+++ middlewares/Statics.js
@@ -7,7 +7,7 @@ module.exports = {
         // Clone the repository
         await exec.execSync(`rm -rf conversel-web`);
         await exec.execSync(
-            `git clone https://mezmerizxd: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@github.com/conversel/conversel-web`
+            `git clone https://username:key@github.com/conversel/conversel-web`
         );
         await exec.execSync(`cd conversel-web && git checkout build`);
     },
